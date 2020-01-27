e531c0285fb26992e9b2fb9f08bc05e6be417acb
yagnapriyad <42752286+yagnapriyad@users.noreply.github.com>
2020-01-27 12:56:27 -0800

    Update filter_raw_data.py

--- src/scripts/filter_raw_data.py
+++ src/scripts/filter_raw_data.py
@@ -4,8 +4,8 @@
 Takes user tweet files from 'raw_data_path', applies filters, and writes output
 to preprocessed_data_path.
 
-June, 2019
-@author: Joshua Rubin
+Jan, 2020
+@author: Yagna
 """
 
 from get_config import (get_config, create_dir_if_not_there)
