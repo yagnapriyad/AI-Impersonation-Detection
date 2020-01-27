c9d10c7c60cf12c56cf93ce170127e4add467f76
yagnapriyad <42752286+yagnapriyad@users.noreply.github.com>
2020-01-27 12:47:57 -0800

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     name="tweetvalidator",
     packages=["tweetvalidator"],
     version="0.0.1",
-    author="Joshua Rubin",
-    author_email="jgrubin@gmail.com",
+    author="Yagna",
+    author_email="priya19290@gmail.com",
     description="Characterizes twitter users' activity and uses that to flag out-of-character content.",
 )
